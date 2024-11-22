0d369ee91829c38a8f44e630bbd2b50e19acfe8b
Dunstere <118548102+Dunstere@users.noreply.github.com>
2024-11-22 19:11:16 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import routes from "./src/routes/postsRoutes.js";
+
+const app = express();
+// Cria uma instância do Express, que será o núcleo da nossa aplicação web.
+routes(app)
+
+app.listen(3000, () => {
+  console.log("Servidor escutando");
+});
+// Inicia o servidor web na porta 3000 e exibe uma mensagem no console quando o servidor estiver ouvindo as requisições.
